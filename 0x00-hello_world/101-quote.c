c678322913700f2ec0e9a8e4a7ec4edba97d038c
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-09-07 11:00:32 -0700

    advance

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <unistd.h>
-
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Description: print without using printf or put
+ * Description: print without using prinyf or put.
  *
- * Return: Always 1 (faill)
+ * Return: 1 (fail)
 */
 int main(void)
 {
-	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, text, 59);
-	return (1);
+char text[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+write(1, text, 59);
+return (1);
 }
