c6edb73c3c1a5bcae581716b3753152c8e555165
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-09-07 04:48:48 -0700

    my firs c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+
+/**
+ * main - the program start here
+ *
+ * Description: This is my C program, that print without 'printf'
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
 int main(void)
 {
-    char text[] = "\"Programming is like building a multilingual puzzle";
-    puts(text);
-    return (0);
+	char text[] = "\"Programming is like building a multilingual puzzle";
+	puts(text);
+	return (0);
+
 }
