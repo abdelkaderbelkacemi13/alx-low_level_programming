0472324760f2dc8b0f907915d00fafcc87319eb7
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-09-08 07:12:18 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:'alphabet list using putchar'
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int alphabet = 'a';
+	
+	for (; 'z' >= alphabet;)
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	return (0);
+}
