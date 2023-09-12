7753e35fafe0cf9e33c86e1971e4dcce16b10307
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-09-12 08:31:27 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,11 @@
 /**
  * _islower - check for lowercase character
  * @low: the character to check
- * Return: 1 if low is lowercase, 0 if not
+ * Return: 1 if low is lowercase, 0 if otherwise
 */
 
 int _islower(int low)
 {
 	if (low >= 97 && low <= 122)
 		return (1);
-	else
-		return (0);
+	return (0);
