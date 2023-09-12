bdd79e315a73072372da5cb181a51480a7c97b1e
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-09-12 03:27:30 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'printing using _putchar prototype'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	ch = 0;
+	while (ch < 8)
+	{
+		_putchar(str[ch]);
+		ch++;
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef TASK_0_MAIN_H
+#define TASK_0_MAIN_H
+int _putchar(char put);
+
+#endif //TASK_0_MAIN_H
