1160305ea6b874adf8c8134324210fa3329c0639
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-09-12 07:21:33 -0700

    print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - it is a function that use _putchar to print the alphabet
+*/
+void print_alphabet(void)
+{
+	int alpha;
+
+	alpha = 'a';
+
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+	_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,13 @@
 #define MAIN_H
 int _putchar(char put);
 void print_alphabet(void);
+void print_alphabet_x10(void);
+
+
+
+
+
+
+
+
 #endif 
