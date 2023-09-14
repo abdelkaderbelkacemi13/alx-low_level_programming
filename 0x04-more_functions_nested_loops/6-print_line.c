c4682632b9e1868646cd22f84505d1e7448c7489
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-09-14 09:17:31 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - draws a line
+ * @n: how many _ printed
+*/
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int numline;
+
+		for (i = numline; numline <= n; numline++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
