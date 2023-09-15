d57b7ec9279927e5074eaec9d731fcdb8a9b75bd
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-09-14 17:06:48 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number - use it to print numbers.
+ * @n: the integer that we will print
+*/
+void print_number(int n)
+{
+	int maxy;
+
+	if (n < 0)
+	{
+		maxy = n;
+	}
+	else
+	{
+		maxy = -n;
+		_putchar('_');
+	}
+	if (maxy / 10)
+	{
+		_putchar(maxy / 10);
+	}
+	_putchar((maxy % 10) + '0');
+}
