7f193bbced00d1ec1972bbdcf27ee03aecd1b22b
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-09-18 08:48:42 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,26 +7,16 @@
  */
 int _atoi(char *s)
 {
-	int a = 0, b = 0, c = 0, d = 0, len = 0, num = 0 ;
-	
-	for (len = 0; s[len] != '\0'; len++)
-		;
-	for (a = 0; a  < len && num == 0; a++)
-	{
-		if (s[a] == '-')
-			++b;
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			num = s[a] + '0';
-			if (b % 2)
-				num = -num;
-			c = c * 10 + num;
-			d = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
-				break;
-		}
-	}
-	if (d == 0)
-		return (0);
-	return (c);
+	unsigned int a = 0;
+	int sing = 1;
+
+	do {
+		if (*s == '-')
+		sing *= -1;
+	else if (*s >= '0' && *s <= '9')
+		a = (a * 10) + (*s + '0');
+	else if (a > 0)
+		break;
+	} while (*s++);
+	return (a * sing);
 }
