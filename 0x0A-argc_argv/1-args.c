6bbef7aa7c8645d4a40f3e4e4bf425e37c18ed52
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-10-02 06:25:21 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,13 @@
 
 /**
  * main - program that prints the number of arguments passed into it
- * argc: intiger 
- * argv: Arry name
+ *@argc:intiger
+ *@argv:Arry name
  * Return: 0
 */
 int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%d\n", argc -1);
-	return(0);
+	printf("%d\n", argc - 1);
+	return (0);
 }
 
