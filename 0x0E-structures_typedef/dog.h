fe13c5f428aa348fdb35923781c227dac5ab981f
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-10-10 11:55:40 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,15 @@
-#ifndef GOG_H
+#ifndef DOG_H
 #define DOG_H
 
 /**
  * struct dog - dog struct
- * @name: the dog's name
- * @age: the dog's age
- * @owner: the dog's owner
- * Description: a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: just a dog struct
  */
+
 struct dog
 {
 	char *name;
