feea1da3b50a140447608c70b0c84f85ebbc3608
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-10-12 11:14:32 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,7 +3,7 @@
 /**
  * print_all - function that prints anything.
  * @format: list of types of arguments passed to the function
- */
+*/
 void print_all(const char * const format, ...)
 {
 	int a = 0;
@@ -15,33 +15,34 @@ void print_all(const char * const format, ...)
 
 	if (format)
 	{
-	while (format[a])
-	{
-		switch (format[a])
+		while (format[a])
 		{
-			case 'c':
-				printf("%s%c", sepa, va_arg(li, int));
-				break;
-			case 'a':
-				printf("%s%d", sepa, va_arg(li, int));
-				break;
-			case 'f':
-				printf("%s%f", sepa, va_arg(li, double));
-				break;
-			case 's':
-				wrd = va_arg(li, char *);
-				if (!wrd)
-					wrd = "(nil)";
-				printf("%s%s", sepa, wrd);
-				break;
-			default:
-				a++;
-				continue;
+			switch (format[a])
+			{
+				case 'c':
+					printf("%s%c", sepa, va_arg(li, int));
+					break;
+				case 'i':
+					printf("%s%d", sepa, va_arg(li, int));
+					break;
+				case 'f':
+					printf("%s%f", sepa, va_arg(li, double));
+					break;
+				case 's':
+					wrd = va_arg(li, char *);
+					if (!wrd)
+						wrd = "(nil)";
+					printf("%s%s", sepa, wrd);
+					break;
+				default:
+					a++;
+					continue;
+			}
+			sepa = ", ";
+			a++;
 		}
-		sepa = ", ";
-		a++;
 	}
-	}
+
 	printf("\n");
 	va_end(li);
 }
