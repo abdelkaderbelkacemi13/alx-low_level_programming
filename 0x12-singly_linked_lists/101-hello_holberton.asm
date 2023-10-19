1bd055781c2ef5170545d4559f0817fb6806ec37
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-10-19 08:17:50 -0700

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,25 +1,12 @@
-section .data
-    format db "Hello, Holberton",0xa,0
-    fmt_len equ $ - format
-
-section .text
-    global main
-    extern printf
+section		.text
+	extern	printf
+	global	main
 
 main:
-    push rdi
-    push rsi
-    push rcx
-    mov rdi, format
-    call printf
-    pop rcx
-    pop rsi
-    pop rdi
+	mov		edi, msg
+	mov		eax, 0
+	call	printf
 
-    mov rax, 60
-    xor rdi, rdi
-    syscall
 
-section .bss
-    resb 8
-
+section		.data
+	msg db 'Hello, Holberton', 0xa, 0
