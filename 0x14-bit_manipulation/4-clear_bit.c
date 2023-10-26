a875973560389bd9f595bf089fb01c164c716a01
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-10-26 07:07:28 -0700

    task 4 update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,13 +9,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index < 0 || index >= sizeof(*n) * 8)
-	{
+	if (index >= sizeof(n) * 8)
 		return (-1);
-	}
-	if (*n & (1L << index))
-	{
-		*n ^= (1L << index);
-	}
+
+	if (*n & 1L << index)
+		*n ^= 1L << index;
 	return (1);
 }
