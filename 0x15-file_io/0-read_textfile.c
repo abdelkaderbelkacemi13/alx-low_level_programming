d92d33a0678e4433b0ff907b7244ec66802c32f9
abdelkaderbelkacemi13 <belkacemi.abdelkader@gmail.com>
2023-10-30 02:55:41 -0700

    first task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,6 +24,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 			bytes = read(fd, &buf[0], letters);
 			bytes = write(STDOUT_FILENO, &buf[0], bytes);
 			close(fd);
+	}
 	return (bytes);
 }
-
